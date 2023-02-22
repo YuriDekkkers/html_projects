ed20369da86ce04e1281be93f861db645d850833
Yuri <2166746@talnet.nl>
2023-02-22 10:46:37 +0100

    its been a while
    alot of files for tests and assignments

---
+++ wackystupiddumbwebsite/JStest.js
@@ -0,0 +1,10 @@
+const age = 18;
+
+if (age >= 18) {
+    console.log("great");
+} else if (age >= 12) {
+    console.log("bit great");
+} else {
+    console.log("ok");
+}
+console.log(typeof("konijn"));
